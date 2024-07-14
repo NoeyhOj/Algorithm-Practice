baea163ac0048fdce43dbdb32405211795655b03
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2024-07-14 21:36:31 +0900

    [Bronze III] Title: 알람 시계, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/2884. 알람 시계/알람 시계.py
+++ Python/백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -2,13 +2,15 @@ import sys
 
 h, m = map(int, sys.stdin.readline().split())
 
-if m - 45 < 0:
-    m = 60 + (m - 45)
+new_m = m - 45
+
+if new_m < 0:
+    m = 60 + new_m
     if h == 0:
         h = 23
     else:
         h = h - 1
 else:
-    m = m - 45
+    m = new_m
 
 print(h, m)
