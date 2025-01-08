47b16875ae12a8b05266805803ed3013c5e6b201
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-01-08 19:35:07 +0900

    [Bronze II] Title: 방 배정하기, Time: 36 ms, Memory: 32412 KB, Score: 8 point -BaekjoonHub

--- Python/백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ Python/백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,16 +1,19 @@
 a,b,c,n = map(int, input().split())
 result = 0
-for x in range(int(n//a)):
-    for y in range(int(n//b)):
-        for z in range(int(n//c)):
-            if((a*x)+(b*y)+(c*z) == n):
-                result = 1
+if(a == 1):
+    print('1')
+else:
+    for x in range(int(n//a)):
+        for y in range(int(n//b)):
+            for z in range(int(n//c)):
+                if((a*x)+(b*y)+(c*z) == n):
+                    result = 1
+                    break
+            if(result == 1):
                 break
         if(result == 1):
             break
     if(result == 1):
-        break
-if(result == 1):
-    print('1')
-else:
-    print('0')
+        print('1')
+    else:
+        print('0')
