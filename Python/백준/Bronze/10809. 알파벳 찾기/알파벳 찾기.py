0a25fa5b465cd4090774aac597735d6d8e95822e
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-01-16 15:52:02 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,5 @@
+spellingList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+word = input()
+for i in range(len(spellingList)):
+    spellingList[i] = str(word.find(spellingList[i]))
+print(' '.join(spellingList))
