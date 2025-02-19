10f1c7cf02f55cca83d32a0220b9ab0b6a728e7c
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-02-19 21:18:47 +0900

    [Bronze IV] Title: Tournament Selection, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/14038. Tournament Selection/Tournament Selection.py
@@ -0,0 +1,13 @@
+count = 0
+for _ in range(6):
+    n = input()
+    if(n == 'W'):
+        count += 1
+if(count >= 5):
+    print(1)
+elif(count >= 3):
+    print(2)
+elif(count >= 1):
+    print(3)
+else:
+    print(-1)
