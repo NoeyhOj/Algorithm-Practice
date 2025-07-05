a524abb84a3adb93172657d2ceaed0baa603c4da
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-07-05 12:29:53 +0900

    [Silver IV] Title: 큐, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+queue = []
+for _ in range(n):
+    command = input().rstrip()
+    if command == 'pop':
+        if len(queue) == 0:
+            print('-1')
+        else:
+            print(queue[0])
+            del queue[0]
+    elif command == 'size':
+        print(len(queue))
+    elif command == 'empty':
+        if len(queue) == 0:
+            print('1')
+        else:
+            print('0')
+    elif command == 'front':
+        if len(queue) == 0:
+            print('-1')
+        else:
+            print(queue[0])
+    elif command == 'back':
+        if len(queue) == 0:
+            print('-1')
+        else:
+            print(queue[-1])
+    else:
+        command, num = command.split()
+        queue.append(num)
