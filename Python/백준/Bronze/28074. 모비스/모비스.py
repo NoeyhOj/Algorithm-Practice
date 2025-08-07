71c16b5425c8c74d182f30b7a29b72c24b3bac34
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-08-07 13:31:33 +0900

    [Bronze IV] Title: 모비스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28074. 모비스/모비스.py
@@ -0,0 +1,9 @@
+import sys
+
+input = sys.stdin.readline
+
+word = list(input().rstrip())
+if 'M' in word and 'O' in word and 'B' in word and 'I' in word and 'S' in word:
+    print("YES")
+else:
+    print("NO")
