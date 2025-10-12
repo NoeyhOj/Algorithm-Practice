644bcbffdeb555d243a6bf2b3c92d27d095c281b
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-12 21:59:49 +0900

    [level 0] Title: n 번째 원소까지, Time: 9.66 ms, Memory: 62.9 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.kt
+++ Kotlin/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(num_list: IntArray, n: Int): IntArray {
         var answer = mutableListOf<Int>()
-        for (i in 0..n-1) {
+        for (i in 0 until n) {
             answer.add(num_list[i])
         }
         return answer.toIntArray()
