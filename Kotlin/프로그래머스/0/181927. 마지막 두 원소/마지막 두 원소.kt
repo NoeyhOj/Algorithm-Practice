4f06c3fd30ab52ae1829dc9d886a3bed77283a98
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-12 22:54:50 +0900

    [level 0] Title: 마지막 두 원소, Time: 12.45 ms, Memory: 66 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = num_list
+        if (num_list[num_list.size - 1] <= num_list[num_list.size - 2]) answer += num_list[num_list.size - 1] * 2 else answer += num_list[num_list.size - 1] - num_list[num_list.size - 2]
+        return answer
+    }
+}
