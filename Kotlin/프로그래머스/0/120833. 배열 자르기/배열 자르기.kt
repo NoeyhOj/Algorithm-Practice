80a89ec92dfcc27d7ba7678153158f08776ba9ea
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-17 15:48:02 +0900

    [level 0] Title: 배열 자르기, Time: 24.50 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/0/120833. 배열 자르기/배열 자르기.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(numbers: IntArray, num1: Int, num2: Int): IntArray {
+        var answer: IntArray = numbers.toList().subList(num1, num2 + 1).toIntArray()
+        return answer
+    }
+}
