674912cac3276191232d2b8e49d88ab49ee6320b
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-17 15:39:43 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 63.5 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.kt
+++ Kotlin/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(array: IntArray, height: Int): Int {
-        var answer: Int = array.filter{ it > height }.size
+        var answer: Int = array.count{ it > height }
         return answer
     }
 }
