88e6ac01fee0479ddf4a26cccb574b018dcfe007
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-18 13:57:17 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(price: Int): Int {
+        var answer = when {
+            price >= 500000 -> price * 0.8
+            price >= 300000 -> price * 0.9
+            price >= 100000 -> price * 0.95
+            else -> price
+        }
+        return answer.toInt()
+    }
+}
