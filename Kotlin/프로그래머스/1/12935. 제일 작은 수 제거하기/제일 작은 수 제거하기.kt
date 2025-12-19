420a19ed0fc8dfaa79a8c3e5d1be05445ce8724c
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-12-19 20:30:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2581.63 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var answer = if (arr.size == 1) {
+            listOf(-1)
+        } else {
+            arr.filter{ it != arr.minOf{ it } }
+        }.toIntArray()
+        return answer
+    }
+}
