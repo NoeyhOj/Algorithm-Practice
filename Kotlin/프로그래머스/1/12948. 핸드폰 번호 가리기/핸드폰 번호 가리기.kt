7bfcf4ebd513e416c215940c191dbc24fbab8d07
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-12-19 13:37:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 10.92 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(phone_number: String): String {
+        var answer = "${"*".repeat(phone_number.length - 4)}${phone_number.substring(phone_number.length-4, phone_number.length)}"
+        return answer
+    }
+}
