70aa0703a7f25f539b311a8b8b469f48418c1015
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-12-26 17:33:06 +0900

    [level 1] Title: 3진법 뒤집기, Time: 9.74 ms, Memory: 64.7 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
+++ Kotlin/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -2,17 +2,7 @@ import kotlin.math.pow
 
 class Solution {
     fun solution(n: Int): Int {
-        var answer: Int = 0
-        var three: String = ""
-        var num: Int = n
-        while (num != 0) {
-            three += (num % 3).toString()
-            num /= 3
-        }
-        three = three.reversed()
-        for (i in three.indices) {
-            answer += (3.0.pow(i) * three[i].digitToInt()).toInt()
-        }
+        var answer: Int = n.toString(3).reversed().toInt(3)
         return answer
     }
 }
