dd10cf0074faf0add2c7aeeb23b0567daf466a36
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-12-26 16:39:43 +0900

    [level 0] Title: 소인수분해, Time: 13.63 ms, Memory: 65 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/120852. 소인수분해/소인수분해.kt
+++ Kotlin/프로그래머스/0/120852. 소인수분해/소인수분해.kt
@@ -2,8 +2,15 @@ import kotlin.math.sqrt
 
 class Solution {
     fun solution(n: Int): IntArray {
-        var answer: IntArray = (2..n).filter{ n % it == 0 }.filter{ num -> 
-            (1..num).filter{ num % it == 0 }.size == 2 }.toIntArray()
+        var answer: IntArray = (2..n).filter{ n % it == 0 && isPrime(it) }.toIntArray()
         return answer
+    }
+    
+    fun isPrime(n: Int): Boolean {
+        if (n < 2) return false
+        for (i in 2..sqrt(n.toDouble()).toInt()) {
+            if (n % i == 0) return false
+        }
+        return true
     }
 }
