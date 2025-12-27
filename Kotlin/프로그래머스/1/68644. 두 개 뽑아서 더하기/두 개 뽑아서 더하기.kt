e4d8efa2d835aa70212b4ca1181bcfb07a77d248
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-12-27 10:32:49 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 17.79 ms, Memory: 65.9 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
+++ Kotlin/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun solution(numbers: IntArray): IntArray {
-        var answer = mutableSetOf<Int>()
-        for (i in (0..numbers.size - 2)) {
-            for (j in ((i+1)..numbers.size - 1)) {
+        val answer = mutableSetOf<Int>()
+        for (i in 0 until numbers.size - 1) {
+            for (j in (i+1) until numbers.size) {
                 answer.add(numbers[i] + numbers[j])
             }
         }
-        return answer.toList().sorted().toIntArray()
+        return answer.sorted().toIntArray()
     }
 }
