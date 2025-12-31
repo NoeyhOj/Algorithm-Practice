b4291d3ceaf23e31cee8dda06b170d3372f0e71c
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-12-31 15:36:04 +0900

    [Bronze III] Title: 네 번째 점, Time: 116 ms, Memory: 20844 KB -BaekjoonHub

--- Kotlin (JVM)/백준/Bronze/3009. 네 번째 점/네 번째 점.kt
+++ Kotlin (JVM)/백준/Bronze/3009. 네 번째 점/네 번째 점.kt
@@ -3,8 +3,5 @@ fun main() {
     repeat(3) {
         nList.add(readln().split(" ").map{ it.toInt() }.toList())
     }
-
-    val x = nList.map{ it[0] }
-    val y = nList.map{ it[1] }
-    println("${x.filter{ num -> x.count{ it == num } == 1}[0]} ${y.filter{ num -> y.count{ it == num } == 1}[0]}")
+    println("${nList.map{ it[0] }.let{ xList -> xList.filter{ num -> xList.count{ it == num } == 1 }[0]}} ${nList.map{ it[1] }.let{ yList -> yList.filter{ num -> yList.count{ it == num } == 1 }[0]}}")
 }
