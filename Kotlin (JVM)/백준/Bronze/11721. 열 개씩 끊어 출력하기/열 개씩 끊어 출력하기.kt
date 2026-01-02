afeb18f78c2ba0f971965b52d5631b99420c0301
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-01-02 12:38:48 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 104 ms, Memory: 15240 KB -BaekjoonHub

--- Kotlin (JVM)/백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.kt
+++ Kotlin (JVM)/백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.kt
@@ -1,14 +1,3 @@
 fun main() {
-    // TODO: 코드 입력
-    val words = mutableListOf<String>()
-    var word = ""
-    readln().forEach{
-        if (word.length == 10) {
-            words.add(word)
-            word = ""
-        }
-        word += it.toString()
-    }
-    words.add(word)
-    println(words.filter{ it != "" }.joinToString("\n"))
+    readln().chunked(10).forEach(::println)
 }
