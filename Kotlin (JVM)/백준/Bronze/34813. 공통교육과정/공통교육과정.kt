ecf97526b41a5c393e6bcc7e8889c43c50870512
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-01-10 12:30:39 +0900

    [Bronze V] Title: 공통교육과정, Time: 100 ms, Memory: 14600 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/34813. 공통교육과정/공통교육과정.kt
@@ -0,0 +1,8 @@
+fun main() {
+    when(readln().first()) {
+        'F' -> println("Foundation")
+        'C' -> println("Claves")
+        'V' -> println("Veritas")
+        'E' -> println("Exploration")
+    }
+}
